e3d92bafc5d875710eadfdbc9b3ce18de7f61be5
Sebastien Merle <sebastien@fluendo.com>
2007-07-16 14:15:44 +0000

    * flumotion/transcoder/admin/admin.py (TranscoderAdmin.__init__): 	* flumotion/transcoder/admin/datastore/activitystore.py (ActivityStore, 	  ActivityStore.__init__, ActivityStore.getLabel, 	  ActivityStore.getTranscodings, ActivityStore.getNotifications, 	  ActivityStore.newTranscoding, ActivityStore.newNotification, 	  ActivityStore._storeActivity, ActivityStore._resetActivity, 	  ActivityStore._deleteActivity, ActivityStore.__getActivities, 	  ActivityStore.__newActivity, ActivityStore.__cbWrapActivities, 	  BaseActivity.store, BaseActivity.reset, BaseActivity.delete, 	  BaseActivity._getAdminStore, BaseActivity._getData, 	  BaseActivity.__ebActivityStoreFailed, 	  BaseActivity.__ebActivityResetFailed, 	  BaseActivity.__ebActivityDeleteFailed, 	  TranscodingActivity.getCustomer, TranscodingActivity.getProfile, 	  TranscodingActivity._setProfile, 	  TranscodingActivity._setInputRelPath, NotificationActivity, 	  NotificationActivity.__init__, NotificationActivity.getRequestURL, 	  NotificationActivity.getRetryCount, NotificationActivity.getRetryMax, 	  NotificationActivity.getRetryNextTime, 	  NotificationActivity.setRetryCount, NotificationActivity.setRetryMax, 	  NotificationActivity.setRetryNextTime, 	  NotificationActivity._setRequestURL, 	  NotificationActivity._setNotification, ActivityFactory): 	Some refactoring. 	The store is now responsible of store and delete operation, 	so the delete and store method do not return deferred, and 	the success of the operation is assumed. If something append 	to fail during these operation it will be the store that 	would be responsible to resolve the problem (retry?)
    svn path=/flumotion-advanced/flumotion-transcoder/trunk/; revision=8013

--- flumotion/transcoder/admin/datastore/activitystore.py
+++ flumotion/transcoder/admin/datastore/activitystore.py
@@ -12,9 +12,82 @@
 
 import datetime
 
+from flumotion.transcoder import log
+
 from flumotion.transcoder.enums import ActivityTypeEnum
 from flumotion.transcoder.enums import ActivityStateEnum
 from flumotion.transcoder.admin.datastore.profilestore import ProfileStore
+from flumotion.transcoder.admin.datastore.notificationstore import BaseNotification
+
+
+class ActivityStore(log.LoggerProxy):
+    
+    def __init__(self, parent, dataSource):
+        self._parent = parent
+        self._dataSource = dataSource
+        
+    def getLabel(self):
+        return "Activity Store"
+        
+    def getTranscodings(self, states):
+        t = ActivityTypeEnum.transcoding
+        return self.__getActivities(t, states)
+
+    def getNotifications(self, states):
+        t = ActivityTypeEnum.notification
+        return self.__getActivities(t, states)
+    
+    def newTranscoding(self, label, state, profile, inputRelPath, 
+                       startTime=None):
+        t = ActivityTypeEnum.transcoding
+        a = self.__newActivity(t, label, state, startTime)
+        a._setProfile(profile)
+        a._setInputRelPath(inputRelPath)
+        return a
+    
+    def newNotification(self, label, state, notification, request,
+                        startTime=None):
+        t = ActivityTypeEnum.notification
+        a = self.__newActivity(t, label, state, startTime)
+        a._setNotification(notification)
+        return a
+
+    
+    ## Protected Methods ##
+    
+    def _storeActivity(self, activity, new):
+        data = activity._getData()
+        return self._dataSource.store(data)
+    
+    def _resetActivity(self, activity):
+        data = activity._getData()
+        return self._dataSource.reset(data)
+
+    def _deleteActivity(self, activity):
+        data = activity._getData()
+        return self._dataSource.delete(data)
+
+    
+    ## Private Methods ##
+
+    def __getActivities(self, type, states):
+        d = self._dataSource.retrieveActivities(type, states)
+        d.addCallback(self.__cbWrapActivities)
+        return d
+    
+    def __newActivity(self, type, label, state, startTime=None):
+        assert isinstance(state, ActivityStateEnum)
+        assert isinstance(label, str)
+        assert (startTime == None) or isinstance(startTime, datetime.datetime)
+        a = self._dataSource.newActivity(type)
+        a.state = state
+        a.label = label
+        a.startTime = startTime or datetime.datetime.now()
+        return ActivityFactory(self, a, True)
+
+    def __cbWrapActivities(self, dataList):
+        return [ActivityFactory(self, d, False) for d in dataList]
+        
 
 class BaseActivity(object):
     
@@ -27,17 +100,18 @@ class BaseActivity(object):
     def store(self):
         assert not self._deleted
         d = self._parent._storeActivity(self, self._isNew)
+        d.addErrback(self.__ebActivityStoreFailed)
         self._new = False
-        return d
-    
-    def reset(self):
-        assert not self._deleted
-        return self._parent._resetActivity(self)
     
     def delete(self):
         assert not self._deleted
         self._deleted = True
-        return self._parent._deleteActivity(self)
+        d = self._parent._deleteActivity(self)
+        d.addErrback(self.__ebActivityDeleteFailed)
+
+    def reset(self):
+        assert not self._deleted
+        return self._parent._resetActivity(self)
         
     def getLabel(self):
         assert not self._deleted
@@ -68,6 +142,9 @@ class BaseActivity(object):
 
     ## Protected Methods ##
     
+    def _getAdminStore(self):
+        return self._parent._parent
+    
     def _touche(self):
         self._data.lastTime = datetime.datetime.now()
     
@@ -75,6 +152,23 @@ class BaseActivity(object):
         return self._data
 
 
+    ## Private Methods ##
+    
+    def __ebActivityStoreFailed(self, failure):
+        self.warning("Fail to store %s activity '%s': %s",
+                     self._data and self._data.type and self._data.type.nick,
+                     self._data and self._data.label,
+                     log.getFailureMessage(failure))
+        self.debug("Activity storing traceback:\n%s",
+                   log.getFailureTraceback(failure))
+        
+    def __ebActivityDeleteFailed(self, failure):
+        self.warning("Fail to delete %s activity '%s': %s",
+                     self._data and self._data.type and self._data.type.nick,
+                     self._data and self._data.label,
+                     log.getFailureMessage(failure))
+        self.debug("Activity deletion traceback:\n%s",
+                   log.getFailureTraceback(failure))
 
 
 class TranscodingActivity(BaseActivity):
@@ -86,33 +180,85 @@ class TranscodingActivity(BaseActivity):
         assert not self._deleted
         return self._data.inputRelPath
     
-    def setInputRelPath(self, relPath):
-        assert not self._deleted
-        assert isinstance(relPath, str)
-        self._data.inputRelPath = relPath
-    
     def getCustomer(self):
         assert not self._deleted
-        return self._parent.getCustomer(self._data.customerName, None)
+        custName = self._data.customerName
+        return self._getAdminStore().getCustomer(custName, None)
     
     def getProfile(self):
         assert not self._deleted
-        cust = self._parent.getCustomer(self._data.customerName, None)
+        custName = self._data.customerName
+        cust = self._getAdminStore().getCustomer(custName, None)
         if not cust:
             return None
         prof = cust.getProfile(self._data.profileName, None)
         return prof
+
+
+    ## Protected Methods ##
     
-    def setProfile(self, profile):
-        assert not self._deleted
+    def _setProfile(self, profile):
         assert isinstance(profile, ProfileStore)
-        self._data.customerName = profile._parent.getName()
+        self._data.customerName = profile.getCustomer().getName()
         self._data.profileName = profile.getName()
-    
-    
-_classLookup = {ActivityTypeEnum.transcoding: TranscodingActivity}
+
+    def _setInputRelPath(self, relPath):
+        assert not self._deleted
+        assert (not relPath) or isinstance(relPath, str)
+        self._data.inputRelPath = relPath
 
 
-def Activity(parent, data, isNew=True):
-    assert data.type in _classLookup
-    return _classLookup[data.type](parent, data, isNew)
+class NotificationActivity(BaseActivity):
+    
+    def __init__(self, parent, data, isNew=True):
+        BaseActivity.__init__(self, parent, data, isNew)
+
+    def getRequestURL(self):
+        assert not self._deleted
+        return self._data.requestURL
+    
+    def getRetryCount(self):
+        assert not self._deleted
+        return self._data.retryCount
+    
+    def getRetryMax(self):
+        assert not self._deleted
+        return self._data.retryMax
+    
+    def getRetryNextTime(self):
+        assert not self._deleted
+        return self._data.retryNextTime
+    
+    def setRetryCount(self, count):
+        assert not self._deleted
+        self._data.retryCount = count
+    
+    def setRetryMax(self, max):
+        assert not self._deleted
+        self._data.retryMax = max
+    
+    def setRetryNextTime(self, time):
+        assert not self._deleted
+        self._data.retryNextTime = time
+    
+    ## Protected Methods ##
+
+    def _setRequestURL(self, url):
+        assert not self._deleted
+        self._data.requestURL = url
+    
+    def _setNotification(self, notification):
+        assert isinstance(notification, BaseNotification)
+        #cust = notification.getCustomer()
+        #prof = notification.getProfile()
+        #self._data.customerName = cust and cust.getName()
+        #self._data.profileName = prof and prof.getName()
+    
+    
+_activityLookup = {ActivityTypeEnum.transcoding: TranscodingActivity,
+                   ActivityTypeEnum.notification: NotificationActivity}
+
+
+def ActivityFactory(parent, data, isNew=True):
+    assert data.type in _activityLookup
+    return _activityLookup[data.type](parent, data, isNew)
