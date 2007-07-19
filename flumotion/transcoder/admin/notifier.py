e77cf34b13d04c87c407a80b31e72d227c28cd40
Sebastien Merle <sebastien@fluendo.com>
2007-07-19 17:19:37 +0000

    * flumotion/transcoder/admin/notifier.py (notifyDebug, 	  Notifier.__init__, Notifier.notify, Notifier._postMail, 	  Notifier._performGetRequest, Notifier.__doPrepareGetRequest, 	  Notifier.__doPrepareMailPost, Notifier.__doPerformGetRequest, 	  Notifier.__cbGetPageSucceed, Notifier.__ebGetPageFailed, 	  Notifier.__doPerformMailPost, Notifier.__cbPostMailSucceed, 	  Notifier.__ebPostMailFailed, Notifier.__retryNotification, 	  Notifier.__notificationSucceed, Notifier.__notificationFailed, 	  Notifier.__notificationTerminated, Notifier.__cannotPrepare, 	  Notifier.__cannotPerform, Notifier.__prepareNotification, 	  Notifier.__performNotification): 	Created the Notifier class that is able to notify by mail or by 	GET requests. For each notification it create and store 	an activitie instance. 	It use twisted.web.client to do the GET request and twisted.smtp 	to send the mails. 	The mails are multipart mime, TO, CC, BCC headers 	and attachments are supported with IDocument.
    svn path=/flumotion-advanced/flumotion-transcoder/trunk/; revision=8066

--- flumotion/transcoder/admin/notifier.py
+++ flumotion/transcoder/admin/notifier.py
@@ -10,14 +10,26 @@
 
 # Headers in this file shall remain intact.
 
-from zope.interface import Interface, implements
-from twisted.internet import reactor, defer
+import email
+import mimetypes
 
-from flumotion.transcoder import log
+from zope.interface import Interface, implements
+from twisted.python.failure import Failure
+from twisted.internet import reactor, defer
+from twisted.web import client
+from twisted.mail.smtp import ESMTPSenderFactory
+
+from flumotion.transcoder import log, utils
 from flumotion.transcoder.admin import adminconsts
+from flumotion.transcoder.admin.errors import NotificationError
 from flumotion.transcoder.admin.enums import ActivityTypeEnum
 from flumotion.transcoder.admin.enums import ActivityStateEnum
+from flumotion.transcoder.admin.enums import NotificationTypeEnum
+from flumotion.transcoder.admin.enums import MailAddressTypeEnum
 from flumotion.transcoder.admin.eventsource import EventSource
+from flumotion.transcoder.admin.datastore.activitystore import BaseNotifyActivity
+from flumotion.transcoder.admin.datastore.activitystore import MailNotifyActivity
+from flumotion.transcoder.admin.datastore.activitystore import GETRequestNotifyActivity
 
 
 ## Notification Function ###
@@ -28,7 +40,6 @@ def notifyEmergency(msg, failure=None):
     emergency situations when no Notifier reference
     is available.
     """
-    pass
 
 def notifyDebug(msg, failure=None):
     """
@@ -36,7 +47,6 @@ def notifyDebug(msg, failure=None):
     debug information (like traceback) when no 
     Notifier reference is available.
     """
-    pass
 
 
 class INotifierListener(Interface):
@@ -54,13 +64,221 @@ class Notifier(log.Loggable,
     logCategory = adminconsts.NOTIFIER_LOG_CATEGORY
     
     def __init__(self, notifierConfig, activityStore):
-        self._store = activityStore
+        self._activities = activityStore
         self._config = notifierConfig
+        self._retries = {} # {BaseNotifyActivity: IDelayedCall}
+        self._results = {} # {BaseNotifyActivity: Deferred}
 
 
     ## Public Methods ##
     
-    def addNotifications(self, notifications):
-        pass
+    def notify(self, label, trigger, notification, variables, documents):
+        self.info("%s notification '%s' [%s] initiated", 
+                  notification.getType().nick, label, trigger.nick)
+        activity = self.__prepareNotification(label, trigger, notification, 
+                                              variables, documents)
+        activity.store()
+        d = defer.Deferred()
+        self._results[activity] = d
+        self._retries[activity] = None
+        self.__performNotification(activity)
+        return d
     
     
+    ## Protected Static Methods ##
+
+    @staticmethod
+    def _postMail(smtpServer, smtpUsername, smtpPassword, 
+                  sender, recipients, bodyFile, 
+                  timeout=None, retries=0):
+        d = defer.Deferred()
+        authenticate = (smtpUsername != None) and (smtpUsername != "")
+        factory = ESMTPSenderFactory(username=smtpUsername,
+                                     password=smtpPassword,
+                                     requireAuthentication=authenticate,
+                                     fromEmail=sender[1], 
+                                     toEmail=[m[1] for m in recipients],
+                                     file=bodyFile, 
+                                     deferred=d,
+                                     retries=retries,
+                                     timeout=timeout)
+        reactor.connectTCP(smtpServer, 25, factory)
+        return d
+
+    @staticmethod
+    def _performGetRequest(url, timeout=None):
+        return client.getPage(url, timeout=timeout,
+                              agent=adminconsts.GET_REQUEST_AGENT)
+    
+    ## Private Methods ##
+    
+    def __doPrepareGetRequest(self, label, trigger, notif, vars, docs):
+        store = self._activities
+        activity = store.newNotification(NotificationTypeEnum.get_request,
+                                         label, ActivityStateEnum.started,
+                                         notif, trigger)
+        url = vars.substitute(notif.getRequestTemplate())
+        activity.setRequestURL(url)
+        return activity
+    
+    def __doPrepareMailPost(self, label, trigger, notif, vars, docs):
+        store = self._activities
+        activity = store.newNotification(NotificationTypeEnum.email,
+                                         label, ActivityStateEnum.started,
+                                         notif, trigger)
+        sender = self._config.mailSender
+        senderAddr = utils.splitMailAddress(sender)[1]
+        activity.setSenderAddr(senderAddr)
+        recipients = notif.getRecipients()
+        allRecipientsAddr = [f[1] for l in recipients.values() for f in l]
+        activity.setRecipientsAddr(allRecipientsAddr)
+        subject = vars.substitute(notif.getSubjectTemplate())
+        activity.setSubject(subject)
+        
+        toRecipientsFields = recipients.get(MailAddressTypeEnum.to, [])
+        toRecipients = utils.joinMailRecipients(toRecipientsFields)
+        ccRecipientsFields = recipients.get(MailAddressTypeEnum.cc, [])
+        ccRecipients = utils.joinMailRecipients(ccRecipientsFields)
+        
+        body = vars.substitute(notif.getBodytemplate())
+
+        msg = email.MIMEMultipart()
+        msg['Subject'] = subject
+        msg['From'] = sender
+        msg['To'] = toRecipients
+        msg['cc'] = ccRecipients
+        txt = email.MIMEText(body)
+        msg.attach(txt)
+        
+        attachments = notif.getAttachments()
+        for doc in docs:
+            if doc.getType() in attachments:
+                mimeType = doc.getMimeType()
+                mainType, subType = mimeType.split('/', 1)
+                data = email.MIMEBase(mainType, subType)
+                data.set_payload(doc.asString())
+                email.Encoders.encode_base64(data)
+                data.add_header('Content-Disposition', 'attachment', 
+                                filename=doc.getLabel())
+                msg.attach(data)
+        activity.setBody(str(msg))
+        
+        return activity
+    
+    def __doPerformGetRequest(self, activity):
+        assert isinstance(activity, GETRequestNotifyActivity)
+        self.debug("GET request '%s' initiated for URL %s" 
+                   % (activity.getLabel(), activity.getRequestURL()))
+        d = self._performGetRequest(activity.getRequestURL(), 
+                                    timeout=activity.getTimeout())
+        args = (activity,)
+        d.addCallbacks(self.__cbGetPageSucceed, self.__ebGetPageFailed,
+                       callbackArgs=args, errbackArgs=args)
+        return d
+
+    def __cbGetPageSucceed(self, page, activity):
+        self.debug("GET request '%s' succeed", activity.getLabel())
+        self.log("GET request '%s' received page:\n%s",
+                 activity.getLabel(), page)
+        self.__notificationSucceed(activity)
+
+    def __ebGetPageFailed(self, failure, activity):
+        self.debug("GET request '%s' failed: %s", activity.getLabel(), 
+                   log.getFailureMessage(failure))
+        self.__retryNotification(activity)
+
+    def __doPerformMailPost(self, activity):
+        assert isinstance(activity, MailNotifyActivity)
+        self.debug("Mail posting '%s' initiated for %s" 
+                   % (activity.getLabel(), activity.getRecipients()))
+        sender = utils.splitMailAddress(activity.getSender())
+        recipients = utils.splitMailRecipients(activity.getRecipients())
+        self.log("Posting mail from %s to %s",
+                 sender[1], ", ".join([r[1] for r in recipients]))
+        d = self._postMail(self._config.smtpServer,
+                           self._config.smtpUsername,
+                           self._config.smtpPassword,
+                           self._config.smtpServer,
+                           sender, recipients,
+                           activity.getBody(),
+                           activity.getTimeout())
+        args = (activity,)
+        d.addCallbacks(self.__cbPostMailSucceed, self.__ebPostMailFailed,
+                       callbackArgs=args, errbackArgs=args)
+        return d
+
+    def __cbPostMailSucceed(self, results, activity):
+        self.debug("Mail post '%s' succeed", activity.getLabel())
+        self.log("Mail post '%s' responses; %s",
+                 activity.getLabel(), ", ".join("'%s': '%s'" % (m, r) 
+                                                for m, c, r in results))
+        self.__notificationFailed(activity)
+    
+    def __ebPostMailFailed(self, failure, activity):
+        self.debug("Mail post '%s' failed: %s", activity.getLabel(), 
+                   log.getFailureMessage(failure))
+        self.__retryNotification(activity)
+
+    def __retryNotification(self, activity):
+        activity.incRetryCount()
+        if activity.getRetryCount() > activity.getRetryMax():
+            desc = "Retry count exceeded (%d)" % activity.getRetryMax()
+            self.__notificationFailed(activity, desc)
+            return
+        activity.store()
+        dc = reactor.callLater(activity.getRetrySleep(),
+                               self.__doPerformNotification,
+                               activity)
+        self._retries[activity] = dc
+        
+    def __notificationSucceed(self, activity):
+        self.info("%s notification '%s' [%s] succeed", 
+                  activity.getSubType().nick, activity.getLabel(), 
+                  activity.getTrigger().nick)
+        activity.setState(ActivityStateEnum.done)
+        activity.store()
+        self._results[activity].callback(activity)
+        self.__notificationTerminated(activity)
+        
+    
+    def __notificationFailed(self, activity, desc=None):
+        message = ("%s notification '%s' [%s] failed: %s", 
+                   activity.getSubType().nick, activity.getLabel(), 
+                   activity.getTrigger().nick, desc)
+        self.info("%s", message)
+        activity.setState(ActivityStateEnum.failed)
+        activity.store()
+        self._results[activity].errback(Failure(NotificationError(message)))
+        self.__notificationTerminated(activity)
+        
+    def __notificationTerminated(self, activity):
+        self._retries.pop(activity)
+        self._results.pop(activity)
+        
+    _prepareLookup = {NotificationTypeEnum.get_request: __doPrepareGetRequest,
+                      NotificationTypeEnum.email: __doPrepareMailPost}
+    
+    _performLookup = {NotificationTypeEnum.get_request: __doPerformGetRequest,
+                      NotificationTypeEnum.email: __doPerformMailPost}
+    
+    def __cannotPrepare(self, label, trigger, notif, vars, docs):
+        self.warnAndRaise(NotificationError, "Unsuported type '%s' for "
+                          "notification '%s'; cannot prepare", 
+                          notif.getType().name, label)
+    
+    def __cannotPerform(self, activity):
+        self.warnAndRaise(NotificationError, "Unsuported type '%s' for "
+                          "notification '%s'; cannot perform", 
+                          activity.getSubtype().name, activity.getLabel())
+    
+    def __prepareNotification(self, label, trigger, notif, vars, docs):
+        type = notif.getType()
+        prep = self._prepareLookup.get(type, self.__cannotPrepare)
+        return prep(label, trigger, notif, vars, docs)
+        
+    def __performNotification(self, activity):
+        self._retries[activity] = None
+        type = activity.getSubtype()
+        prep = self._performLookup.get(type, self.__cannotPerform)
+        prep(activity)
+        
