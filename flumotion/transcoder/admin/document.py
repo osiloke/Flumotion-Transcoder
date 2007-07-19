7dc93fa131a8166472133260d20f29b9b220ee85
Sebastien Merle <sebastien@fluendo.com>
2007-07-19 17:16:33 +0000

    * flumotion/transcoder/admin/Makefile.am: 	* flumotion/transcoder/admin/document.py (IDocument, 	  IDocument.getLabel, IDocument.getType, IDocument.getMimeType, 	  IDocument.__str__, IDocument.asString, IDocument.asFile, 	  StringDocument, StringDocument.__init__, StringDocument.getLabel, 	  StringDocument.getType, StringDocument.getMimeType, 	  StringDocument.__str__, StringDocument.asString, 	  StringDocument.asFile, FileDocument, FileDocument.__init__, 	  FileDocument.getLabel, FileDocument.getType, 	  FileDocument.getMimeType, FileDocument.__str__, 	  FileDocument.asString, FileDocument.asFile): 	Added a document wrapper to encapsulate the report, config and log files. 	This way generic document can be passed to the notifier for it 	to choose what to do with them. A document have a type 	(DocumentTypeEnum) a mime type, a label to identify it and 	can be retrieved as string or file.
    svn path=/flumotion-advanced/flumotion-transcoder/trunk/; revision=8050

---
+++ flumotion/transcoder/admin/document.py
@@ -0,0 +1,113 @@
+# -*- Mode: Python -*-
+# vi:si:et:sw=4:sts=4:ts=4
+#
+# Flumotion - a streaming media server
+# Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
+# All rights reserved.
+
+# Licensees having purchased or holding a valid Flumotion Advanced
+# Streaming Server license may use this file in accordance with the
+# Flumotion Advanced Streaming Server Commercial License Agreement.
+# See "LICENSE.Flumotion" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+
+import os
+import mimetypes
+from cStringIO import StringIO
+
+
+from zope.interface import Interface, implements
+
+from flumotiuon.transcoder.admin.errors import DocumentError
+from flumotiuon.transcoder.admin.enums import DocumentTypeEnum
+
+
+class IDocument(Interface):
+    
+    def getLabel(self):
+        pass
+    
+    def getType(self):
+        pass
+    
+    def getMimeType(self):
+        pass
+
+    def __str__(self):
+        pass
+    
+    def asString(self):
+        pass
+    
+    def asFile(self):
+        pass
+
+    
+class StringDocument(object):
+    
+    implements(IDocument)
+    
+    def __init__(self, type, label, data, mimeType='text/plain'):
+        assert isinstance(type, DocumentTypeEnum)
+        self._type = type
+        self._label = label
+        self._data = data
+        self._mime = mimeType
+    
+    def getLabel(self):
+        return self._label
+    
+    def getType(self):
+        return self._type
+    
+    def getMimeType(self):
+        return self._mime
+    
+    def __str__(self):
+        return self._data
+    
+    def asString(self):
+        return self._data
+    
+    def asFile(self):
+        return StringIO(self._data)
+
+
+class FileDocument(object):
+    
+    implements(IDocument)
+    
+    def __init__(self, type, label, path, mimeType=None):
+        assert isinstance(type, DocumentTypeEnum)
+        if not os.path.exists(path):
+            raise DocumentError("File document '%s' not found", path)
+        self._label = label or os.path.basename(self._path)
+        self._type = type
+        self._path = path
+        self._mime = mimeType or  mimetypes.guess_type(path)[0]
+        if not self._mime:
+            self._mime = 'application/octet-stream'
+    
+    def getLabel(self):
+        return self._label
+    
+    def getType(self):
+        return self._type
+    
+    def getMimeType(self):
+        return self._mime
+    
+    def __str__(self):
+        return self.asString()
+    
+    def asString(self):
+        f = open(self._path)
+        try:
+            return f.read()
+        finally:
+            f.close()
+    
+    def asFile(self):
+        return open(self._path)
+
