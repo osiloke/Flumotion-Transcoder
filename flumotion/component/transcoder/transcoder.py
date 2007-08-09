1e0ae45f54d3f752afb5189d299127556588b458
Sebastien Merle <sebastien@fluendo.com>
2007-08-09 12:08:06 +0000

    * flumotion/component/transcoder/transcoder.py: 	Removed wrong import statment.
    svn path=/flumotion-advanced/flumotion-transcoder/trunk/; revision=8311

--- flumotion/component/transcoder/transcoder.py
+++ flumotion/component/transcoder/transcoder.py
@@ -4,7 +4,6 @@
 # Flumotion - a streaming media server
 # Copyright (C) 2004,2005,2006,2007 Fluendo, S.L. (www.fluendo.com).
 # All rights reserved.
-from turtle import position
 
 # Licensees having purchased or holding a valid Flumotion Advanced
 # Streaming Server license may use this file in accordance with the
