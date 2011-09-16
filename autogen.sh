4063b107d751c059d745f983f5d310c6e124a1ff
Xavier Queralt <xqueralt@flumotion.com>
2011-09-16 10:58:47 +0200

    Add flumotion-common submodule

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 set -x
+
+# Make sure we have common
+if test ! -f common/flumotion-trial;
+  then
+  echo "+ Setting up common submodule"
+  git submodule init
+fi
+git submodule update
+
+# source helper functions
+if test ! -f common/flumotion-trial;
+  then
+  echo There is something wrong with your source tree.
+  echo You are missing common/flumotion-trial
+  exit 1
+fi
+
 aclocal -I common || exit 1
 # libtoolize --force || exit 1
 # autoheader || exit 1
